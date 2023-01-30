dbf3f4bdc67d117f2d5575662b629f6c886fe424
Pawel <studentpawel2000@gmail.com>
2023-01-30 13:48:22 +0100

    hosting

--- src/Kernel.php
+++ src/Kernel.php
@@ -8,4 +8,9 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+        /**
+     * NOTICE:
+     * We don't want to deploy composer.json that's being used to compute project dir, so we will specify it here manually.
+     */
+    
 }
